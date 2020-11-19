a8b312ae8d51653289c301b146b31e9d8b4db699
MikheevDesign <mikheevsergey82@gmail.com>
2020-11-19 23:49:31 +0700

    Добавил вывод вопросов на главную

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\QuestionRepository;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main")
      */
-    public function index(): Response
+    public function index(QuestionRepository $questionRepository): Response
     {
+		$question = $questionRepository->findBy([],['created' => 'DESC']);
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'question' => $question,
         ]);
     }
 }
